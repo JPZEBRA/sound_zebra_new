b345d804fe33b22da1fe8503f4bcb1b01a56a353
JPZEBRA <84015964+JPZEBRA@users.noreply.github.com>
2025-02-18 10:23:27 +0900

    Add files via upload

--- sound_base/FM/sound_FM_envelope.py
+++ sound_base/FM/sound_FM_envelope.py
@@ -10,6 +10,8 @@ FME_A2 = 0
 FME_A3 = 0
 FME_A4 = 0
 
+FME_C1 = 0.002
+
 # SOUND LEVEL 0-100
 
 def set_FME_level(L1,L2,L3,L4) :
@@ -38,70 +40,72 @@ def set_FME_poly(A1,A2,A3,A4) :
     FME_A3 = A3
     FME_A4 = A4
 
+def set_FME_speed(speed,sampling) :
+
+    global FME_C1
+
+    FME_C1 = 0.002 * 441000 / sampling
+
 def set_FME(sound,duration):
 
     s = np.zeros(duration)
 
-    mode = 0
-    cts = 0
-    count = duration
+    count = [0,0,0,0,0]
+
+    if FME_A1== 0 or FME_A1>= 90 :
+        count[0] = 0
+    else :
+        count[0] = int(np.abs(FME_L1 - FME_L4)/np.tan(np.pi*FME_A1/180))/FME_C1
+    if FME_A2 == 0 or FME_A2 >= 90 :
+        count[1] = count[0]
+    else :
+        count[1] = count[0] + int(np.abs(FME_L2 - FME_L1)/np.tan(np.pi*FME_A2/180))/FME_C1
+    if FME_A3 == 0 or FME_A3 >= 90 :
+        count[2] = count[1]
+    else :
+        count[2] = count[1] + int(np.abs(FME_L3 - FME_L2)/np.tan(np.pi*FME_A3/180))/FME_C1
+    if FME_A4 == 0 or FME_A4 >= 90 :
+        count[3] = duration
+    else :
+        count[3] = duration - int(np.abs(FME_L4 - FME_L3)/np.tan(np.pi*FME_A4/180))/FME_C1
+
+    count[4] = duration
 
     level = FME_L4
+
+    mode = 0
+
+    if FME_A1 == 0 or FME_A1 >= 90 : level = FME_L1
 
     for n in range(duration) :
 
         s[n] = level/100
 
-        if mode >= 3 :
-            if n >= count : mode = 4
-        else :
-            cts = cts + 1
-            if cts >= count :
-                mode = mode + 1
-                if mode == 1 : level = FME_L1
-                if mode == 2 : level = FME_L2
-                if mode == 3 : level = FME_L3
- 
+        if n >= count[mode] : mode = mode + 1
+
         if mode == 0 :
-            if FME_A1 == 0 or FME_A1 >= 90 :
-                mode == 1
-                count = 0
-                level = FME_L1
+            if( count[0] > 0 ) :
+                level = (FME_L1 - FME_L4)/ count[0] * n + FME_L4
             else :
-                level = level + np.sign(FME_L1 - FME_L4)*np.sin(np.pi*FME_A1/180)*100/duration
-                count = int(np.abs(FME_L1 - FME_L4)/np.tan(np.pi*FME_A1/180))*duration/100
-            continue
+                mode = mode + 1
         if mode == 1 :
-            if FME_A2 == 0 or FME_A2 >= 90 :
-                mode == 2
-                count = 0
-                level = FME_L2
+            if( count[1] - count[0] > 0 ) :
+                level = (FME_L2 - FME_L1)/(count[1]-count[0]) * ( n - count[0]) + FME_L1
             else :
-                level = level + np.sign(FME_L2 - FME_L1)*np.sin(np.pi*FME_A2/180)*100/duration
-                count = int(np.abs(FME_L2 - FME_L1)/np.tan(np.pi*FME_A2/180))*duration/100
-            continue
+                mode = mode + 1
         if mode == 2 :
-            if FME_A3 == 0 or FME_A3 >= 90 :
-                mode == 3
-                count = 0
-                level = FME_L3
+            if( count[2] - count[1] > 0 ) :
+                level = (FME_L3 - FME_L2)/(count[2]-count[1]) * ( n - count[1]) + FME_L2
             else :
-                level = level + np.sign(FME_L3 - FME_L2)*np.sin(np.pi*FME_A3/180)*100/duration
-                count = int(np.abs(FME_L3 - FME_L2)/np.tan(np.pi*FME_A3/180))*duration/100
-            continue
-        if mode == 3 :
-            if FME_A4 == 0 or FME_A4 >= 90 :
-                count = duration
-            else :
-                count = duration - int(np.abs(FME_L4 - FME_L3)/np.tan(np.pi*FME_A4/180))*duration/100
-            continue
+                mode = mode + 1
         if mode == 4 :
-                level = level + np.sign(FME_L4 - FME_L3)*np.sin(np.pi*FME_A4/180)*100/duration
+            if( count[4] - count[3] > 0 ) :
+                level = (FME_L4 - FME_L3)/(count[4]-count[3]) * ( n - count[3]) + FME_L3
+            else :
+                mode = mode + 1
+        if mode == 5 :
+            level = FME_L4
 
- 
-
-    for n in range(duration) :
-
-        s[n] = s[n]*sound[n]
+    s = s * sound
 
     return s
